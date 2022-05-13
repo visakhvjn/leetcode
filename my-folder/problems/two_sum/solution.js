6739fd731eb3d79f563a4e26c22c7b8eb917e950
Visakh Vijayan <vjnvisakh@gmail.com>
2022-05-13 07:41:55 +0000

    Sync LeetCode submission - Two Sum (javascript)

---
+++ my-folder/problems/two_sum/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    const length = nums.length;
+    
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+};
