744f0c81bbf7ed7396b0ace8c0aaa947e8280b35
Visakh Vijayan <vjnvisakh@gmail.com>
2023-11-02 14:18:35 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (typescript)

---
+++ my-folder/problems/remove_duplicates_from_sorted_array/solution.ts
@@ -0,0 +1,12 @@
+function removeDuplicates(nums: number[]): number {
+    let i = 0;
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            nums[i + 1] = nums[j];
+            i++;
+        }
+    }
+
+    return i + 1;
+};
