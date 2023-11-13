6b893a24398b50358e573df6192b7b5c938420e3
Visakh Vijayan <vjnvisakh@gmail.com>
2023-11-13 14:12:19 +0000

    Sync LeetCode submission - Merge Sorted Array (typescript)

---
+++ my-folder/problems/merge_sorted_array/solution.ts
@@ -0,0 +1,30 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let ptr = nums1.length - 1;
+
+    while (m > 0 && n > 0) {
+        if (nums1[m - 1] > nums2[n - 1]) {
+            nums1[ptr] = nums1[m - 1];
+            m -= 1;
+        } else {
+            nums1[ptr] = nums2[n - 1];
+            n -= 1;
+        }
+
+        ptr -= 1;
+    }
+
+    while (m > 0) {
+        nums1[ptr] = nums1[m - 1];
+        ptr -= 1;
+        m -= 1;
+    }
+
+    while (n > 0) {
+        nums1[ptr] = nums2[n - 1];
+        ptr -= 1;
+        n -= 1;
+    }
+};
